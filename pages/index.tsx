742f92f7cbf0c542bc16ddc05874af6344bf50fe
John McNeill <jh_mcneill@yahoo.ca>
2022-08-29 15:39:28 -0600

    basic ui structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div className="">Hello world</div>;
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-center text-2xl">Which Pokemon is rounder?</div>
+      <div className="p-2" />
+      <div className="border p-8 rounded flex items-center justify-between max-w-2xl">
+        <div className="h-16 w-16 bg-red-300" />
+        <div className="p-8">vs</div>
+        <div className="h-16 w-16 bg-red-800" />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
