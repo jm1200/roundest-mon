30e16455d449bb0b358a7c856342f7ff4d9a9f3b
John McNeill <jh_mcneill@yahoo.ca>
2022-08-29 14:41:44 -0600

    default styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div className="text-2xl text-red-200">Hello world</div>;
+  return <div className="">Hello world</div>;
 };
 
 export default Home;
